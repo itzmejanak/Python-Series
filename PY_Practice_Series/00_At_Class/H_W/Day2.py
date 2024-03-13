d34203da222831a62b84775192715f666c679fdb
Janak Devkota <itzmejanak@gmail.com>
2024-03-13 10:33:28 +0545

    🎉 init:

--- PY_Practice_Series/00_At_Class/H_W/Day2.py
+++ PY_Practice_Series/00_At_Class/H_W/Day2.py
@@ -13,14 +13,17 @@ def calculate_rent():
     print("\n\tWelcome to Room Rent Calculator!")
     print("-----------------------------------------")
     print("Please provide the following details to proceed:\n")
+    print(type(print()))
 
     # Creating a flag to control the while loop
     exit_requested = False
+    print(type(exit_requested))
 
     # Loop continues until the user decides to exit
     while not exit_requested:
         # Asking the user to provide the number of days
         asked_days = float(input("How many days do you want to book this room? "))
+        print(type(asked_days))
 
         # Checking if the input is valid
         if asked_days < 1:
@@ -30,21 +33,28 @@ def calculate_rent():
             # Assigning the value of the weekly rent and per-day rent
             room_price1 = 5000  # Weekly rent
             room_price2 = 1000  # Per-day rent
+            print(type(room_price1))
 
             # Calculating the number of full weeks and remaining days
             weeks = asked_days // 7  # only weeks
             remainder_days = asked_days % 7
+            print(type(weeks))
+            print(type(remainder_days))
 
             # Calculating rent based on weekly and daily rates
             rent1 = weeks * room_price1  # Rent for full weeks
             rent2 = remainder_days * room_price2  # Rent for remaining days
+            print(type(rent1))
+            print(type(rent2))
 
             # Calculating total rent
             total_rent = rent1 + rent2
             print("Total rent is Rs:", total_rent)
+            print(type(total_rent))
 
             # Asking the user if they want to quit
             choice = input("Do you want to quit? \"Y/N\": ").lower()
+            print(type(choice))
             if choice == "y":
                 exit_requested = True
 
@@ -57,33 +67,15 @@ calculate_rent()
 
 # Q2. Solve one problem from the book referred from chapter 2 or 5 or 7!
 
-# Rental Car (Task 7-1):
-def rental_car():
-    car_choice = input("What kind of rental car would you like? ")
-    print(f"Let me see if I can find you a {car_choice}.")
-
-rental_car()
-
-
-
 # Restaurant Seating (Task 7-2):
 def restaurant_seating():
     group_size = int(input("How many people are in your dinner group? "))
+    print(type(group_size))
     if group_size > 8:
         print("I'm sorry, you'll have to wait for a table.")
     else:
         print("Your table is ready.")
+        print(type(print()))
 
 restaurant_seating()
 
-
-
-# Multiples of Ten (Task 7-3):
-def multiples_of_ten():
-    number = int(input("Please enter a number: "))
-    if number % 10 == 0:
-        print(f"{number} is a multiple of 10.")
-    else:
-        print(f"{number} is not a multiple of 10.")
-
-multiples_of_ten()
